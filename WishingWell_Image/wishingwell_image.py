d1442c886356eed687d681de7b94c91b3f3ab57c
Jonathan Sanderson <jonathan@quernstone.com>
2016-04-17 11:59:51 +0100

    Non-working Circular buffer: doesn't init with non-h264 capture

--- WishingWell_Image/wishingwell_image.py
+++ WishingWell_Image/wishingwell_image.py
@@ -91,155 +91,157 @@ with picamera.PiCamera() as camera:
 
     # Now loop, within context of this camera object. Ooh.
     
-    while 1:
-        # Handle PyGame events (ie. keypress controls)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            elif event.type is pygame.KEYDOWN:
-                key_press = pygame.key.name(event.key)
-                print key_press
-                if key_press is "s":
-                    camera.shutter_speed -= 1000
-                    if camera.shutter_speed < shutter_min:
-                        camera.shutter_speed = shutter_min
-                    print "Shutter speed set to: %s" % camera.shutter_speed
-                elif key_press is "w":
-                    camera.shutter_speed += 1000
-                    if camera.shutter_speed > shutter_max:
-                        camera.shutter_speed = shutter_max
-                    print "Shutter speed set to: %s" % camera.shutter_speed
-                elif key_press is "e":
-                    threshold_low += 1
-                    if threshold_low > 255:
-                        threshold_low = 255
-                    print "threshold_low set to %i" % threshold_low
-                elif key_press is "d":
-                    threshold_low -= 1
-                    if threshold_low < 0:
-                        threshold_low = 0
-                    print "threshold_low set to %i" % threshold_low
-                elif key_press is "r":
-                    threshold_high += 1
-                    if threshold_high > 255:
-                        threshold_high = 255
-                    print "threshold_high set to %i" % threshold_high
-                elif key_press is "f":
-                    threshold_high -= 1
-                    if threshold_high < 0:
-                        threshold_high = 0
-                    print "threshold_high set to %i" % threshold_high
-                elif key_press is "m":
-                    # Toggle output mode
-                    output_mode += 1
-                    if output_mode > 3:
-                        output_mode = 1
-                    
-                # Check for left shift and allow rapid threshold changes
-                if pygame.key.get_mods() & pygame.KMOD_LSHIFT:
-                    if key_press is "e":
-                        threshold_low += 10
-                        if threshold_low > 255:
-                            threshold_low = 255
-                        print "threshold_low set to %i" % threshold_low
-                    elif key_press is "d":
-                        threshold_low -= 10
-                        if threshold_low < 0:
-                            threshold_low = 0
-                        print "threshold_low set to %i" % threshold_low
-                    elif key_press is "r":
-                        threshold_high += 10
-                        if threshold_high > 255:
-                            threshold_high = 255
-                        print "threshold_high set to %i" % threshold_high
-                    elif key_press is "f":
-                        threshold_high -= 10
-                        if threshold_high < 0:
-                            threshold_high = 0
-                        print "threshold_high set to %i" % threshold_high
-                    # Check for SHIFT+P and if found, set working image to pure black again
-                    elif key_press is "p":
-                        print "*** STARTING OVER ***"
-                        composite = Image.frombytes('RGB', size, "\x00" * width * height * 3)
-                        composite = composite.convert('RGBA')
-    
-        with Timer() as t1:
-            with picamera.array.PiRGBArray(camera, size=(width, height)) as stream:
-                stream.truncate(0) # Nuke the existing stream and repopulate for new frame
-                with Timer() as t_capture:
-                    camera.capture(stream, 'rgb', use_video_port=video_port) # Capture YUV data to stream object
-                    print "Frame %s captured" % frame_count
-                print "==> Frame capture in %s s" % t_capture.secs
-                with Timer() as t_rgb:
-                    frame = Image.fromarray(stream.array, "RGB") # Grab the YUV data into a Pillow YCbCr image
-                print "==> Frame from array to RGB in %s s" % t_rgb.secs
-                with Timer() as t_convertYUV:
-                    frameyuv = frame.convert("YCbCr")
-                    frameyuv_array = np.array(frameyuv)
-                    y = frameyuv_array[0:width,0:height,0] # Get just the y component as a numpy array
-                print "==> YUV conversion & Y extraction in %s s" % t_convertYUV.secs
-        print "=> Frame capture complete in %s s" % t1.secs
-    
-        # Output overall brightness calculation. Later, we'll use this to 
-        # Handle flash frame discrimination
-        print "frame brightness: %d" % (get_brightness(frame))
-    
-        # with Timer() as t2:
-    	    # frame = transparentify(frame, threshold)
-        # print "=> Frame transparentified in %s s" % t2.secs
-    
-        with Timer() as t2a:
-			#~ mask = Image.fromarray(y, "L")
-			#~ mask.save("mask-before.jpeg")
-			#~ print "MAX MASK VALUE: %s" % np.amax(y)
-			#~ print "MIN MASK VALUE: %s" % np.amin(y)
+    # MAIN LOOP START
+    with picamera.PiCameraCircularIO(camera, seconds=2) as stream:
+        camera.start_recording(stream, format='h264')
+        print "CAMERA RECORDING"
+        camera.wait_recording(1)
+        print "1-SECOND BUFFER RECORDED"
+        try:
+            while True:
+                # Handle PyGame events (ie. keypress controls)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT: sys.exit()
+                    elif event.type is pygame.KEYDOWN:
+                        key_press = pygame.key.name(event.key)
+                        print key_press
+                        if key_press is "s":
+                            camera.shutter_speed -= 1000
+                            if camera.shutter_speed < shutter_min:
+                                camera.shutter_speed = shutter_min
+                            print "Shutter speed set to: %s" % camera.shutter_speed
+                        elif key_press is "w":
+                            camera.shutter_speed += 1000
+                            if camera.shutter_speed > shutter_max:
+                                camera.shutter_speed = shutter_max
+                            print "Shutter speed set to: %s" % camera.shutter_speed
+                        elif key_press is "e":
+                            threshold_low += 1
+                            if threshold_low > 255:
+                                threshold_low = 255
+                            print "threshold_low set to %i" % threshold_low
+                        elif key_press is "d":
+                            threshold_low -= 1
+                            if threshold_low < 0:
+                                threshold_low = 0
+                            print "threshold_low set to %i" % threshold_low
+                        elif key_press is "r":
+                            threshold_high += 1
+                            if threshold_high > 255:
+                                threshold_high = 255
+                            print "threshold_high set to %i" % threshold_high
+                        elif key_press is "f":
+                            threshold_high -= 1
+                            if threshold_high < 0:
+                                threshold_high = 0
+                            print "threshold_high set to %i" % threshold_high
+                        elif key_press is "m":
+                            # Toggle output mode
+                            output_mode += 1
+                            if output_mode > 3:
+                                output_mode = 1
+                        
+                    # Check for left shift and allow rapid threshold changes
+                    if pygame.key.get_mods() & pygame.KMOD_LSHIFT:
+                        if key_press is "q":
+                            sys.exit()
+                        if key_press is "e":
+                            threshold_low += 10
+                            if threshold_low > 255:
+                                threshold_low = 255
+                            print "threshold_low set to %i" % threshold_low
+                        elif key_press is "d":
+                            threshold_low -= 10
+                            if threshold_low < 0:
+                                threshold_low = 0
+                            print "threshold_low set to %i" % threshold_low
+                        elif key_press is "r":
+                            threshold_high += 10
+                            if threshold_high > 255:
+                                threshold_high = 255
+                            print "threshold_high set to %i" % threshold_high
+                        elif key_press is "f":
+                            threshold_high -= 10
+                            if threshold_high < 0:
+                                threshold_high = 0
+                            print "threshold_high set to %i" % threshold_high
+                        # Check for SHIFT+P and if found, set working image to pure black again
+                        elif key_press is "p":
+                            print "*** STARTING OVER ***"
+                            composite = Image.frombytes('RGB', size, "\x00" * width * height * 3)
+                            composite = composite.convert('RGBA')
 
-			# Index Y array for values below threshold_low
-            low_clip_indices = y < threshold_low
-            # Set values at those indices to zero (ie. transparent)
-            y[low_clip_indices] = 0
-            # Index y array for values above threshold_high
-            high_clip_indices = y > threshold_high
-            # Set values at those indices to 255 (ie. solid)
-            y[high_clip_indices] = 255
-            #~ y_clip = np.clip(y, 0, threshold)
-            #~ print "POST-CLIP MAX MASK VALUE: %s" % np.amax(y)
-            #~ print "POST-CLIP MIN MASK VALUE: %s" % np.amin(y)
-            # Make mask image from Numpy array y
-            mask = Image.fromarray(y, "L")
-            #~ mask.save("mask-after.jpeg")
-        #~ print "=> Clip processing %s s" %t2a.secs
-    
-        with Timer() as t3:
-            # Convert captured frame to RGBA
-            frame = frame.convert("RGBA")
-            #~ print frame.format, frame.size
-            #~ print mask.format, mask.size
-            # Combine captured frame with rolling composite, via computed mask
-    	    composite.paste(frame, (0,0), mask)
-        #~ print "=> Frame composited in %s s" % t3.secs
-    
-        # Prepare the PyGame surface
-        # Need to convert PIL image to string representation, then string to PyGame image. Ugh.
-        # Mode switching doesn't work for now, but the stub is here.
-        if output_mode is 1:
-            # Default mode: display composite
-            raw_str = composite.tostring("raw", 'RGBA')
-            pygame_surface = pygame.image.fromstring(raw_str, size, 'RGBA')
-        elif output_mode is 2:
-            # Display mask only. Doesn't work.
-            raw_str = mask.tostring("raw", 'L')
-            pygame.surface = pygame.image.fromstring(raw_str, size, 'P')
-        elif output_mode is 3:
-            # Display raw image (no composite). Doesn't work.
-            raw_str = frame.tostring("raw", 'RGBA')
-            pygame.surface = pygame.image.fromstring(raw_str, size, 'RGBA')
+                with Timer() as t_capture:
+                    # Lock the stream briefly
+                    with stream.lock:
+                        # Just grab the first image, whatever it is
+                        stream.seek(0)
+                        capture_frame = Image.open(stream) 
+                        # stream.clear() # Reset the circular stream, so we never see this image again
+
+                    # We have our frame, so we've unlocked the circularIO stream
+                    # ...which merrily continues on a background thread. We hope.
+
+                    # So now we can process the image
+                    frameyuv = capture_frame.convert("YCbCr")
+                    frameyuv_array = np.array(frameyuv)
+                    y = frameyuv_array[0:width, 0:height, 0] # Get just the y component as a numpy array
+                print "=> Capture & conversion done in %d s" % t_capture
+
+                # Output overall brightness calculation. Later, we'll use this to 
+                # Handle flash frame discrimination
+                print "frame brightness: %d" % (get_brightness(frame))
         
-        # Finally, update the window
-        screen.blit(pygame_surface, (0,0))
-        pygame.display.flip()
-        
-        frame.close() # release the file handle. Should avoid the memory leak?
-        # (I don't think there is a memory leak any more, but I daren't remove this without testing)
-        frame_count += 1
-        # and around we go again.
+
+    			#~ mask = Image.fromarray(y, "L")
+    			#~ mask.save("mask-before.jpeg")
+    			#~ print "MAX MASK VALUE: %s" % np.amax(y)
+    			#~ print "MIN MASK VALUE: %s" % np.amin(y)
+
+    			# Index Y array for values below threshold_low
+                low_clip_indices = y < threshold_low
+                # Set values at those indices to zero (ie. transparent)
+                y[low_clip_indices] = 0
+                # Index y array for values above threshold_high
+                high_clip_indices = y > threshold_high
+                # Set values at those indices to 255 (ie. solid)
+                y[high_clip_indices] = 255
+                #~ y_clip = np.clip(y, 0, threshold)
+                #~ print "POST-CLIP MAX MASK VALUE: %s" % np.amax(y)
+                #~ print "POST-CLIP MIN MASK VALUE: %s" % np.amin(y)
+                # Make mask image from Numpy array y
+                mask = Image.fromarray(y, "L")
+                #~ mask.save("mask-after.jpeg")
     
+                # Convert captured frame to RGBA
+                frame = frame.convert("RGBA")
+                #~ print frame.format, frame.size
+                #~ print mask.format, mask.size
+                # Combine captured frame with rolling composite, via computed mask
+                composite.paste(frame, (0,0), mask)
+    
+                # Prepare the PyGame surface
+                # Need to convert PIL image to string representation, then string to PyGame image. Ugh.
+                # Mode switching doesn't work for now, but the stub is here.
+                if output_mode is 1:
+                    # Default mode: display composite
+                    raw_str = composite.tostring("raw", 'RGBA')
+                    pygame_surface = pygame.image.fromstring(raw_str, size, 'RGBA')
+                elif output_mode is 2:
+                    # Display mask only. Doesn't work.
+                    raw_str = mask.tostring("raw", 'L')
+                    pygame.surface = pygame.image.fromstring(raw_str, size, 'P')
+                elif output_mode is 3:
+                    # Display raw image (no composite). Doesn't work.
+                    raw_str = frame.tostring("raw", 'RGBA')
+                    pygame.surface = pygame.image.fromstring(raw_str, size, 'RGBA')
+                
+                # Finally, update the window
+                screen.blit(pygame_surface, (0,0))
+                pygame.display.flip()
+                
+                frame_count += 1
+                # and around we go again.
+
+        finally:
+            camera.stop_recording()
