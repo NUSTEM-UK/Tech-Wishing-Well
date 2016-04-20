515e9d688d5658dfcf4568e99ecb811c8cbafa33
Jonathan Sanderson <jonathan@quernstone.com>
2016-04-20 10:14:03 +0100

    Toggle fullscreen mode, pygame event cleanups

--- WishingWell_Image/wishingwell_image.py
+++ WishingWell_Image/wishingwell_image.py
@@ -25,7 +25,7 @@ script, file_prefix = argv
 # Best frame-rate compromise for a 1920x1080 display is probably 864 
 size = width, height = 864, 864
 # Should we display full screen, or windowed?
-full_screen = 0
+full_screen = 1
 
 # Video settings
 # Keep the video_framerate low to allow longer shutter speeds.
@@ -120,95 +120,100 @@ def handlePygameEvents():
     global overmask_size
     global overmask_centre
     global overmask_radius
+    global full_screen
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type is pygame.KEYDOWN:
-            key_press = pygame.key.name(event.key)
+            key_press = event.key
+            # key_press = pygame.key.name(event.key)
             # print key_press # For diagnostic purposes, but messes up output
-            if key_press is "s":
+            if key_press == pygame.K_s:
                 if (camera.shutter_speed - 1000) >= shutter_min:
                     camera.shutter_speed -= 1000
                 print "Shutter speed set to: %s" % camera.shutter_speed
-            elif key_press is "w":
+            elif key_press == pygame.K_w:
                 if (camera.shutter_speed + 1000) <= shutter_max:
                     camera.shutter_speed += 1000
                 print "Shutter speed set to: %s" % camera.shutter_speed
-            elif key_press is "e":
+            elif key_press == pygame.K_e:
                 if (threshold_low + 1) < 256:
                     threshold_low += 1
                 print "threshold_low set to %i" % threshold_low
-            elif key_press is "d":
+            elif key_press == pygame.K_d:
                 if (threshold_low - 1) >= 0:
                     threshold_low -= 1
                 print "threshold_low set to %i" % threshold_low
-            elif key_press is "r":
+            elif key_press == pygame.K_r:
                 if (threshold_high + 1) < 256:
                     threshold_high += 1
                 print "threshold_high set to %i" % threshold_high
-            elif key_press is "f":
+            elif key_press == pygame.K_f:
                 if (threshold_high -1) >= 0:
                     threshold_high -= 1
                 print "threshold_high set to %i" % threshold_high
-            elif key_press is "m":
-                # Toggle output mode
-                output_mode += 1
-                if output_mode > 3:
-                    output_mode = 1
-            elif key_press is "i":
+            elif key_press == pygame.K_m:
+                if full_screen:
+                    pygame.display.set_mode(size)
+                    pygame.mouse.set_visible(1)
+                else:
+                    pygame.display.set_mode(size, pygame.FULLSCREEN)
+                    pygame.mouse.set_visible(0)
+                full_screen = not full_screen
+            elif key_press == pygame.K_i:
                 if ( overmask_centre[1] - 10 ) > 0:
                     overmask_centre[1] -= 10
                     drawOvermask()
-            elif key_press is "k":
+            elif key_press == pygame.K_k:
                 if ( overmask_centre[1] + 10 ) < overmask_size[1]:
                     overmask_centre[1] += 10
                     drawOvermask()
-            elif key_press is "j":
+            elif key_press == pygame.K_j:
                 if ( overmask_centre[0] - 10 ) > 0:
                     overmask_centre[0] -= 10
                     drawOvermask()
-            elif key_press is "l":
+            elif key_press == pygame.K_l:
                 if ( overmask_centre[0] + 10 ) < overmask_size[0]:
                     overmask_centre[0] += 10
                     drawOvermask()
-            elif key_press is "y":
+            elif key_press == pygame.K_y:
                 overmask_radius += 10
                 drawOvermask()
-            elif key_press is "h":
+            elif key_press == pygame.K_h:
                 overmask_radius -= 10
                 drawOvermask()
-            elif key_press is "o":
+            elif key_press == pygame.K_o:
                 framedump()
             
             # Check for left shift and allow rapid threshold changes
             if pygame.key.get_mods() & pygame.KMOD_LSHIFT:
-                if key_press is "q":
+                if key_press == pygame.K_q:
                     sys.exit()
-                if key_press is "e":
+                if key_press == pygame.K_e:
                     if (threshold_low + 10) < 256:
                         threshold_low += 10
                     print "threshold_low set to %i" % threshold_low
-                elif key_press is "d":
+                elif key_press == pygame.K_d:
                     if (threshold_low - 10) >= 0:
                         threshold_low -= 10
                     print "threshold_low set to %i" % threshold_low
-                elif key_press is "r":
+                elif key_press == pygame.K_r:
                     if (threshold_high + 10) < 256:
                         threshold_high += 10
                     print "threshold_high set to %i" % threshold_high
-                elif key_press is "f":
+                elif key_press == pygame.K_f:
                     if (threshold_high -10) >= 0:
                         threshold_high -= 10
                     print "threshold_high set to %i" % threshold_high
-                elif key_press is "s":
+                elif key_press == pygame.K_s:
                     if (camera.shutter_speed - 10000) >= shutter_min:
                         camera.shutter_speed -= 10000
                     print "Shutter speed set to: %s" % camera.shutter_speed
-                elif key_press is "w":
+                elif key_press == pygame.K_w:
                     if (camera.shutter_speed + 10000) <= shutter_max:
                         camera.shutter_speed += 10000
                     print "Shutter speed set to: %s" % camera.shutter_speed
                 # Check for SHIFT+P and if found, set working image to pure black again
-                elif key_press is "p":
+                elif key_press == pygame.K_p:
                     print "*** STARTING OVER ***"
                     composite = Image.frombytes('RGB', size, "\x00" * width * height * 3)
                     composite = composite.convert('RGBA')
