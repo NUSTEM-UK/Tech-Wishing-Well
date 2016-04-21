c615d9d94e55542c7efb7769e46bb30b40be9b83
llewmihs <joeshimwell@gmail.com>
2016-04-21 20:04:31 +0000

    Added some generic tweets

--- WishingWell_Twitter/generic_tweets.py
+++ WishingWell_Twitter/generic_tweets.py
@@ -1,3 +1,13 @@
 genericList = [
-    "Generic Maker Faire Tweet."
+    "I'm having a great time at #makerfaireuk.",
+    "I couldn't be having more fun at #makerfaireuk",
+    "Generic message about the great time I'm having at #makerfaireuk",
+    "Look at me! I'm at #makerfaireuk",
+    "I've made an LED throwie to cast in the Technology Wishing Well @ #makerfaireuk",
+    "#havingthebesttimeatmakerfaireuk",
+    "Making a wish for a future technology at #makerfaireuk",
+    "I wish, I wish, I wish for... #makerfaireuk",
+    "Look what I made at #makerfaireuk",
+    "I'm ready to cast my tech wish at #makerfaireuk",
+    "Having a #awesome time at #makerfaireuk"
 	]
