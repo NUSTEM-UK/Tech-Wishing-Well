e11e8ee25f5456ac017592067450442c25723b6b
Joe <joeshimwell@gmail.com>
2017-03-03 10:18:29 +0000

    Adjusted code further for Raspberry Pi Bday Bash

--- WishingWell_Twitter/WishingWell_TwitterPIBDAY.py
+++ WishingWell_Twitter/WishingWell_TwitterPIBDAY.py
@@ -1,10 +1,9 @@
 from gpiozero import LED, Button
-import random
 import time
-from twython import Twython
+import random
+from twython import Twython, TwythonError
 import picamera
 from random_proverbs import proverbList
-
 
 try:
     from stat import *
@@ -16,31 +15,30 @@ except:
     pass
 
 # define the pins for the buttons
-snapButton = Button(23)
-selectButton = Button(24)
+snapButton = Button(24, bounce_time = 1)
+selectButton = Button(23, bounce_time = 1)
 
 # define the pins for the LEDs
-topLED = LED(5)
-middleLED = LED(6)
-bottomLED = LED(13)
+topLED = LED(13)
+middleLED = LED(5)
+bottomLED = LED(6)
 
 # initial message and light config
 topLED.on()
 middleLED.off()
 bottomLED.off()
-message = "Having a blast @raspberry_pi birthday bash 2017!"
+message = "Having a blast @raspberrypi birthday bash 2017!"
 location = 1
 
 # config the Twitter API client
 config={}
 execfile("realconfig.py", config)
+print(config["app_key"])
 twitter = Twython(config["app_key"],config["app_secret"],config["oauth_token"],config["oauth_token_secret"])
 
 # config the camera
 camera = picamera.PiCamera()
 camera.led=False
-camera.rotation = 270
-camera.resolution = (1360, 768)
 camera.start_preview()
 
 def tww_image_getter():
@@ -60,71 +58,75 @@ def tww_image_getter():
 
 def take_photo():
     camera.led = True
-    time.sleep(0.7)
+    time.sleep(1)
     camera.annotate_text = '3'
-    time.sleep(0.7)
+    time.sleep(1)
     camera.annotate_text = '2'
-    time.sleep(0.7)
+    time.sleep(1)
     camera.annotate_text = '1'
-    time.sleep(0.7)
+    time.sleep(1)
     camera.annotate_text = 'Smile!'
-    time.sleep(0.4)
+    time.sleep(0.7)
     camera.annotate_text = ''
     camera.led = False
+    camera.resolution = (1360, 768)
+    print("Taking photo")
     camera.capture("picamimg.jpg")
 
-def twitter_upload(message, path):
+def twitter_upload(message, filep):
     if message == "random":
-        message = proverbList[random.randint(0,len(proverbList))-1] + " Wow this Tech Wishing Well is deep!"
-    photo = open(path, 'rb')
+        message = proverbList[random.randint(0,len(proverbList))-1] + ". Wow this Tech Wishing Well is deep!"
+    photo = open(filep, 'rb')
     response = twitter.upload_media(media = photo)
+    print("Uploading tweet")
     try:
         twitter.update_status(status = message, media_ids=[response['media_id']])
-    except:
-        print("Upload failed")
-
+        print("Done uploading")
+    except Twythonerror as e:
+        print(e.error_code)
+        
 def LEDconfig(location):
     if location == 1:
         topLED.on()
         middleLED.off()
         bottomLED.off()
-        message = "Having a blast @raspberry_pi birthday bash 2017! Here's my tech wish for the future..."
+        message = "Having a blast @raspberrypi birthday bash 2017!"
         return message
     elif location == 2:
         topLED.off()
         middleLED.on()
         bottomLED.off()
-        message = "Looking forward to @makerfaireuk on the 1st and 2nd April in Newcastle! Here's my tech wish for the future..."
+        message = "Looking forward to @makerfaireuk 2017 on the 1st and 2nd April in Newcastle!"
         return message
     else:
         topLED.off()
         middleLED.off()
         bottomLED.on()
         message = "random"
-        #message = "Wow these people from @nustemuk are cool! Here's my tech wish for the future..."
         return message
+try:
+    while True:
+        tww_image_getter()
+        if snapButton.is_pressed:
+            print("Pressed 1")
+            take_photo()
+            twitter_upload(message, "picamimg.jpg")
+            
+        elif selectButton.is_pressed:
+            print("Pressed 2")
+            time.sleep(0.2)
+            if location == 1:
+                message = LEDconfig(1)
+                print(message)
+                location = 2
+            elif location == 2:
+                message = LEDconfig(2)
+                print(message)
+                location = 3
+            else:
+                message = LEDconfig(3)
+                print(message)
+                location = 1
 
-def main():
-    try:
-        while True:
-            if snapButton.is_pressed:
-                take_photo()
-                twitter_upload(message, "picamimg.jpg")
-
-            elif selectButton.is_pressed:
-                time.sleep(0.2)
-                if location == 1:
-                    message = LEDconfig(1)
-                    location = 2
-                elif location == 2:
-                    message = LEDconfig(2)
-                    location = 3
-                else:
-                    message = LEDconfig(3)
-                    location = 1
-
-    finally:
-        camera.stop_preview()
-
-if __name__ == "__main__":
-    main()
+finally:
+    camera.stop_preview()
---
+++ WishingWell_Twitter/realconfig.py
@@ -0,0 +1,6 @@
+#Details for Twitter
+
+app_key = "9EpKmsuYBXs115n8vSODZrCU0"
+app_secret = "krsco1y756Ik8cuMi96aQOjgrrIXjQSYS4xljRd44WhCXysUIt"
+oauth_token = "718059190644826112-KkhWa04DGu0rpOGVgQiHTFOp1JlJNJ2"
+oauth_token_secret = "GamxfRNBmnHOiZ9IPZx6jnJSTjn7vPUn0UbtaF5DXOoKV"
