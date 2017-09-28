d0bf468f0c08b65176ba2b0bacad09d113d36907
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2017-09-28 10:40:54 +0100

    Another approach, from the forums

---
+++ WishingWell_Image/cameratest/test10.py
@@ -0,0 +1,20 @@
+# Based on forum code by OutoftheBOTS:
+# https://www.raspberrypi.org/forums/viewtopic.php?f=63&t=194059&sid=c31bbc7bbc9276a185b4498da04acf52#p1215709
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+camera.resolution = (640, 360)
+rawCapture = PiRGBArray(camera, size=(640, 360))
+time.sleep(0.1)
+
+start = time()
+#cycle through the stream of images from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array #the frame will be stroed in the varible called image
+	#add your processing code here to process the image array
+    elapsed = time() - start
+    print(elapsed)
